02fa8507f1711ff3f9f9a8d9136f46a02e34d765
PavlushaSource <pavlushkin.dima1785@gmail.com>
2025-06-07 20:47:02 +0300

    feat: Add rollback methods
    squash

--- internal/adapters/kubeController.go
+++ internal/adapters/kubeController.go
@@ -8,6 +8,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/retry"
 	"log/slog"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -152,4 +153,106 @@ func (ctrl *KubeRuntimeController) Start(ctx context.Context) error {
 	ctrl.mgr = mgr
 
 	return err
+}
+
+func (ctrl *KubeRuntimeController) GetAvailableRevisions(ctx context.Context, deployName, nameSpace string) ([]string, error) {
+	var deployment v1.Deployment
+	err := ctrl.client.Get(ctx, client.ObjectKey{
+		Namespace: nameSpace,
+		Name:      deployName,
+	}, &deployment)
+	if err != nil {
+		slog.Error("Failed to get deployment", "name", deployName, "namespace", nameSpace, "error", err)
+		return nil, fmt.Errorf("failed to get deployment %s: %w", deployName, err)
+	}
+
+	var replicaSetList v1.ReplicaSetList
+	err = ctrl.client.List(ctx, &replicaSetList, &client.ListOptions{
+		Namespace: nameSpace,
+	})
+	if err != nil {
+		slog.Error("Failed to list replicasets", "namespace", nameSpace, "error", err)
+		return nil, fmt.Errorf("failed to list replicasets in namespace %s: %w", nameSpace, err)
+	}
+
+	revisions := make([]string, 0)
+	for _, rs := range replicaSetList.Items {
+		for _, owner := range rs.OwnerReferences {
+			if owner.Kind == "Deployment" && owner.Name == deployName {
+				revision, ok := rs.Annotations["deployment.kubernetes.io/revision"]
+				if ok {
+					revisions = append(revisions, revision)
+				}
+			}
+		}
+	}
+
+	return revisions, nil
+}
+func (ctrl *KubeRuntimeController) SetRevision(ctx context.Context, deployName, namespace string, revision string) error {
+	var deployment v1.Deployment
+	err := ctrl.client.Get(ctx, client.ObjectKey{
+		Namespace: namespace,
+		Name:      deployName,
+	}, &deployment)
+	if err != nil {
+		slog.Error("Failed to get deployment", "name", deployName, "namespace", namespace, "error", err)
+		return fmt.Errorf("failed to get deployment %s: %w", deployName, err)
+	}
+
+	var rsList v1.ReplicaSetList
+	err = ctrl.client.List(ctx, &rsList, &client.ListOptions{
+		Namespace: namespace,
+	})
+	if err != nil {
+		slog.Error("Failed to list replicasets", "namespace", namespace, "error", err)
+		return fmt.Errorf("failed to list replicasets in namespace %s: %w", namespace, err)
+	}
+
+	var targetRS *v1.ReplicaSet
+	for _, rs := range rsList.Items {
+		for _, owner := range rs.OwnerReferences {
+			if owner.Kind == "Deployment" && owner.Name == deployName {
+				if rs.Annotations["deployment.kubernetes.io/revision"] == revision {
+					targetRS = rs.DeepCopy()
+					break
+				}
+			}
+		}
+		if targetRS != nil {
+			break
+		}
+	}
+
+	if targetRS == nil {
+		slog.Warn("No ReplicaSet found with the specified revision", "revision", revision)
+		return fmt.Errorf("no ReplicaSet found with revision %s", revision)
+	}
+
+	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		var updatedDeployment v1.Deployment
+		err = ctrl.client.Get(ctx, client.ObjectKey{
+			Namespace: namespace,
+			Name:      deployName,
+		}, &updatedDeployment)
+		if err != nil {
+			return fmt.Errorf("failed to get latest deployment %s: %w", deployName, err)
+		}
+		updatedDeployment.Spec.Template = targetRS.Spec.Template
+		if updatedDeployment.Annotations == nil {
+			updatedDeployment.Annotations = make(map[string]string)
+		}
+		updatedDeployment.Annotations["kubernetes.io/change-cause"] = fmt.Sprintf("Rollback to revision %s", revision)
+		if err = ctrl.client.Update(ctx, &updatedDeployment); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		slog.Error("Failed to set revision", "revision", revision, "error", err)
+		return fmt.Errorf("failed to set revision %s: %w", revision, err)
+	}
+	slog.Info("Successfully set revision", "revision", revision, "deployment", deployName)
+
+	return nil
 }
