b61f145d7ac533b1f7d5e6c6ca4198c6095e1c52
Artem Demchenko <artdem243@gmail.com>
2025-06-07 18:33:50 +0300

    setup UI

--- app/tg/tg.go
+++ app/tg/tg.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 	"hack-a-tone/internal/core/port"
+	"log/slog"
+	"strconv"
 	"strings"
 )
 
@@ -55,34 +58,63 @@ func (b *Bot) start() {
 	updates, _ := b.bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {
+		if update.Message == nil || update.Message.Text == "" {
 			continue
 		}
 
-		var msg tgbotapi.MessageConfig
+		switch strings.ToLower(update.Message.Text) {
+		case "/start":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет ! Я создан для того, чтобы ..."+
+				"\nСконфигурируй систему, с которой хочешь работать")
+			msg.ReplyMarkup = startScreen
+			b.bot.Send(msg)
 
-		if update.Message.Text != "" {
-			switch strings.ToLower(update.Message.Text) {
-			case "/start":
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Привет ! Я создан для того, чтобы ..."+
-					"\nСконфигурируй систему, с которой хочешь работать")
-				msg.ReplyMarkup = startScreen
+		case "вернуться":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выбери, что хочешь сделать:")
+			msg.ReplyMarkup = startScreen
+			b.bot.Send(msg)
 
-			case "вернуться":
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Выбери, что хочешь сделать:")
-				msg.ReplyMarkup = startScreen
+		case "придумать название кнопки для действий надо бы":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выбери действие, которое хочешь сделать")
+			msg.ReplyMarkup = someActionButtons
+			b.bot.Send(msg)
 
-			case "придумать название кнопки для действий надо бы":
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Выбери действие, которое хочешь сделать")
-				msg.ReplyMarkup = someActionButtons
-				fmt.Println("im here")
-
-			default:
+		case "посмотреть данные о системе":
+			pods, err := b.k8sController.GetAllPods(context.Background())
+			if err != nil {
+				slog.Error("Не удалось получить все поды", err)
 			}
+			out := make([]string, len(pods.Items))
+			for i, v := range pods.Items {
+				out[i] = fmt.Sprintf("%d: %s", i+1, v.Status.Phase)
+			}
+			str := strings.Join(out, "\n")
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, str)
+			//msg.ReplyMarkup = someActionButtons
+			b.bot.Send(msg)
 
-		} else {
-			//msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Я не умею такое обрабатывать :(\nВоспользуйся подсказками !")
+		case "добавить мощностей":
+			ask := tgbotapi.NewMessage(update.Message.Chat.ID, "Сколько подиков добавить?")
+			ask.ReplyMarkup = tgbotapi.ForceReply{ForceReply: true} // шаг 1 :contentReference[oaicite:0]{index=0}
+			askedPodsId, _ := b.bot.Send(ask)
+			for listen := range updates {
+				listenMessage := listen.Message
+				if listenMessage != nil && listenMessage.ReplyToMessage != nil &&
+					listenMessage.ReplyToMessage.MessageID == askedPodsId.MessageID {
+					u32, err := strconv.ParseUint(listenMessage.Text, 10, 32)
+					if err != nil {
+						newAsk := tgbotapi.NewMessage(update.Message.Chat.ID, "Введи целое беззнаковое число")
+						newAsk.ReplyMarkup = tgbotapi.ForceReply{ForceReply: true}
+						askedPodsId, _ = b.bot.Send(newAsk)
+						continue
+					}
+					newAsk := tgbotapi.NewMessage(listen.Message.Chat.ID, fmt.Sprintf("Добавлено %d подиков", u32))
+					b.bot.Send(newAsk)
+				} else {
+					break
+				}
+			}
+		default:
 		}
-		b.bot.Send(msg)
 	}
 }
